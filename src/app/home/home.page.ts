b7b26f54e3c1be0712fee19f481776a2dac1aade
natashazywczyk <128709275+natashazywczyk@users.noreply.github.com>
2025-04-14 13:01:25 +0000

    now copies global css

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,12 +7,22 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor(private router: Router) {}
 
+  ngOnInit() {
+    // Check localStorage for the saved theme state
+    const savedTheme = localStorage.getItem('dark-mode');
+    const isDark = savedTheme === 'true'; // Convert string to boolean
+
+    // Apply the theme globally
+    document.documentElement.classList.toggle('ion-palette-dark', isDark);
+  }
+
   goToTheme() {
-    this.router.navigate(['/theme']); // Navigates to the theme page
+    this.router.navigate(['/theme']); // Navigates tso sthe theme page
   }
 
 }
+
