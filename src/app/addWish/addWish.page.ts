fc1c9a433aec1673200903fba9b34a0e868242fe
natashazywczyk <128709275+natashazywczyk@users.noreply.github.com>
2025-04-14 14:28:03 +0000

    stores date data

--- src/app/addWish/addWish.page.ts
+++ src/app/addWish/addWish.page.ts
@@ -7,7 +7,18 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AddWishPage {
+  dateAdded: string = ''; // Property to store the selected date
+  startDate: string = '';
+  endDate: string = '';
 
-  constructor() {}
-
+  constructor() {
+    // Set the minimum date to today
+    const today = new Date();
+    this.startDate = today.toISOString().split('T')[0]; // Format as YYYY-MM-DD, as calender reads it in this format
+    
+     // Set the maximum date to a future date
+     const futureDate = new Date(2050, 11, 31);
+     this.endDate = futureDate.toISOString().split('T')[0]; // Format as YYYY-MM-DD, as calender reads it in this format
+   }
 }
+
