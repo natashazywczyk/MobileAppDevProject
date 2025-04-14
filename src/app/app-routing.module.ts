7d2d2cfe93e25cfcd4109bc9ec9f0ffd71243446
natashazywczyk <128709275+natashazywczyk@users.noreply.github.com>
2025-04-14 12:30:34 +0000

    allow routing for back button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomePageModule),
+  },
+  {
+    
     path: 'theme',
     loadChildren: () => import('./theme/theme.module').then( m => m.ThemePageModule)
   }
