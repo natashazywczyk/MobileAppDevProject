974e71c5ab2f241fceb3b1fa5bf4b69e08b9057b
natashazywczyk <128709275+natashazywczyk@users.noreply.github.com>
2025-04-14 11:41:35 +0000

    theme setting page added

---
+++ src/app/theme/theme.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-theme',
+  templateUrl: './theme.page.html',
+  styleUrls: ['./theme.page.scss'],
+})
+export class ThemePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
