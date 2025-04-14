bd9564dd0bc58b48ceab76cb1bb57e58571eb840
natashazywczyk <128709275+natashazywczyk@users.noreply.github.com>
2025-04-14 15:00:03 +0000

    loads saved theme

--- src/app/bucketlist/bucketlist.page.ts
+++ src/app/bucketlist/bucketlist.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
@@ -6,8 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['bucketlist.page.scss'],
   standalone: false,
 })
-export class BucketListPage {
+export class BucketListPage implements OnInit {
 
   constructor() {}
 
+
+  ngOnInit() {
+    // Check localStorage for the saved theme state
+    const savedTheme = localStorage.getItem('dark-mode');
+    const isDark = savedTheme === 'true'; // Convert string to boolean
+
+    // Apply the theme globally
+    document.documentElement.classList.toggle('ion-palette-dark', isDark);
+  }
 }
