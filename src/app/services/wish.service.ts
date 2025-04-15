6523c5dbc4acd35f36d13209b9145fa34df602ad
natashazywczyk <128709275+natashazywczyk@users.noreply.github.com>
2025-04-15 13:36:51 +0000

    added observables for add, get and delete wish

--- src/app/services/wish.service.ts
+++ src/app/services/wish.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WishService {
-  private wishes: any[] = []; // Array to store wishes
+  private dbUrl = 'http://localhost:4000/api/wishes'; // Backend API URL
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   // Add a new wish
-  addWish(wish: any) {
-    this.wishes.push(wish);
+  addWish(wish: any): Observable<any> {
+    return this.http.post(this.dbUrl, wish); // Send POST request to the backend
   }
 
   // Get all wishes
-  getWishes() {
-    return this.wishes;
+  getWishes(): Observable<any[]> {
+    return this.http.get<any[]>(this.dbUrl);
+  }
+
+  // Delete a wish by ID
+  deleteWish(id: string): Observable<any> {
+    return this.http.delete(`${this.dbUrl}/${id}`);
   }
 }
