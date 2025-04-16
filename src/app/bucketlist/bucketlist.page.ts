351619955b859e401db390e96bca4206b92ac3c9
NATASHA ZYWCZYK <G00415696@atu.ie>
2025-04-16 15:32:23 +0100

    imported refresh service to call refresh event after wish added

--- src/app/bucketlist/bucketlist.page.ts
+++ src/app/bucketlist/bucketlist.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WishService } from '../services/wish.service';
 import { firstValueFrom } from 'rxjs';
+import { RefreshService } from '../services/refresh.service'; 
 
 @Component({
   selector: 'app-tab3',
@@ -11,13 +12,19 @@ import { firstValueFrom } from 'rxjs';
 export class BucketListPage implements OnInit {
   wishes: any[] = []; // Array to store wishes
 
-  constructor(private wishService: WishService) {}
+  constructor(private wishService: WishService, private refreshService: RefreshService) {}
 
   ngOnInit() {
-    // Load all wishes on initialization
+    //Load all wishes when page is loaded
     this.loadWishes();
+
+    //Look out for refresh
+    this.refreshService.getRefreshObservable().subscribe(() => {
+      this.reloadData();
+    });
   }
 
+  //Function to load wishes from backend server using WishService
   async loadWishes() {
     try {
       const data = await firstValueFrom(this.wishService.getWishes());
@@ -26,6 +33,10 @@ export class BucketListPage implements OnInit {
     catch (error) {
       console.error('Error fetching wishes:', error);
     }
+  }
+
+  reloadData() {
+    this.loadWishes(); // Refresh the list of wishes
   }
 
   async deleteWish(id: string) {
